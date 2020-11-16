d9b915333621c14d3b09b5e09b602cff809d91eb
bt161013 <theo.brentot@etu.univ-lehavre.fr>
2020-11-16 15:56:49 +0100

    Modification de la classes Wallet pour utiliser la classe Decimal2

--- src/com/company/Wallet.java
+++ src/com/company/Wallet.java
@@ -4,15 +4,20 @@ import java.util.*;
 
 public class Wallet
 {
+
     // tableau contenant le nombre de pièce(s)/billet(s) que l'on peut utiliser au maximum
     int[] tabmoneyLimite = new int[15];
     //tableau contenant le nombre de pièce(s)/billet(s) rendu
     int[] tabmoney = new int[15];
 
-    // La variable money contient le prix donner par l'utilisateur, rendu contient le prix rendu par le programme et compter contient la même chose que money mais permet de fair les calculs dans les boucles sans toucher à money
-    double money, rendu, compter;
+    // La variable money contient le prix donner par l'utilisateur
+    double money;
     //tableau contenant la valeur des différents pièces/billets
-    double[] tabprix = new double[] {500, 200, 100, 50, 20, 10, 5, 2, 1, 0.50, 0.20, 0.10, 0.05, 0.02, 0.01};
+    double[] tabprix = new double[] {500.00, 200.00, 100.00, 50.00, 20.00, 10.00, 5.00, 2.00, 1.00, 0.50, 0.20, 0.10, 0.05, 0.02, 0.01};
+
+    Decimal2 compter = new Decimal2();
+    Decimal2 rendu = new Decimal2();
+    Decimal2 moneyValue = new Decimal2();
 
     public static void main(String[] args)
     {
@@ -20,7 +25,7 @@ public class Wallet
         Wallet w = new Wallet();
 
         // La variable money contient le prix donner par l'utilisateur, rendu contient le prix rendu par le programme et compter contient la même chose que money mais permet de fair les calculs dans les boucles sans toucher à money
-        w.rendu = 0.00;
+        w.rendu.setNumber(0.00);
 
         System.out.print("Donner un prix :");
         // Demande à l'utilisateur de saisir un prix
@@ -35,7 +40,7 @@ public class Wallet
         }
 
         w.money = sc.nextDouble();
-        w.compter = w.money;
+        w.compter.setNumber(w.money);
 
         // Remplissage du tableau tabmoneyLimite
         w.iniTabMoneyLimite();
@@ -46,105 +51,96 @@ public class Wallet
             w.tabmoney[i] = 0;
         }
 
+        double transaction;
+
         // Boucle pour vérifier si on à rendu le prix exacte
-        while (w.rendu != w.money & w.compter > 0.00)
+        while (w.rendu.getNumber() != w.money & w.compter.getNumber() > 0.00)
         {
+            transaction = w.compter.getNumber();
             // Vérifie s'il reste au moins 500 euros à rendre
-            if (w.compter - 500.00 >= 0.00 & w.pieceDisponible(0))
+            if (transaction >= w.tabprix[0] & w.pieceDisponible(0))
             {
                 w.calcul(0);
             }
             // Vérifie s'il reste au moins 200 euros à rendre
-            else if (w.compter - 200.00 >= 0.00 & w.pieceDisponible(1))
+            else if (transaction >= w.tabprix[1] & w.pieceDisponible(1))
             {
                 w.calcul(1);
             }
             // Vérifie s'il reste au moins 100 euros à rendre
-            else if (w.compter - 100.00 >= 0.00 & w.pieceDisponible(2))
+            else if (transaction >= w.tabprix[2] & w.pieceDisponible(2))
             {
                 w.calcul(2);
             }
             // Vérifie s'il reste au moins 50 euros à rendre
-            else if (w.compter - 50.00 >= 0.00 & w.pieceDisponible(3))
+            else if (transaction >= w.tabprix[3] & w.pieceDisponible(3))
             {
                 w.calcul(3);
             }
             // Vérifie s'il reste au moins 20 euros à rendre
-            else if (w.compter - 20.00 >= 0.00 & w.pieceDisponible(4))
+            else if (transaction >= w.tabprix[4] & w.pieceDisponible(4))
             {
                 w.calcul(4);
             }
             // Vérifie s'il reste au moins 10 euros à rendre
-            else if (w.compter - 10.00 >= 0.00 & w.pieceDisponible(5))
+            else if (transaction >= w.tabprix[5] & w.pieceDisponible(5))
             {
                 w.calcul(5);
             }
             // Vérifie s'il reste au moins 5 euros à rendre
-            else if (w.compter - 5.00 >= 0.00 & w.pieceDisponible(6))
+            else if (transaction >= w.tabprix[6] & w.pieceDisponible(6))
             {
                 w.calcul(6);
             }
             // Vérifie s'il reste au moins 2 euros à rendre
-            else if (w.compter - 2.00 >= 0.00 & w.pieceDisponible(7))
+            else if (transaction >= w.tabprix[7] & w.pieceDisponible(7))
             {
                 w.calcul(7);
             }
             // Vérifie s'il reste au moins 1 euro à rendre
-            else if (w.compter - 1.00 >= 0.00 & w.pieceDisponible(8))
+            else if (transaction >= w.tabprix[8] & w.pieceDisponible(8))
             {
                 w.calcul(8);
             }
             // Vérifie s'il reste au moins 0,50 euro à rendre
-            else if (w.compter - 0.50 >= 0.00 & w.pieceDisponible(9))
+            else if (transaction >= w.tabprix[9] & w.pieceDisponible(9))
             {
                 w.calcul(9);
             }
             // Vérifie s'il reste au moins 0,20 euro à rendre
-            else if (w.compter - 0.20 >= 0.00 & w.pieceDisponible(10))
+            else if (transaction >= w.tabprix[10] & w.pieceDisponible(10))
             {
                 w.calcul(10);
             }
             // Vérifie s'il reste au moins 0,10 euro à rendre
-            else if (w.compter - 0.10 >= 0.00 & w.pieceDisponible(11))
+            else if (transaction >= w.tabprix[11] & w.pieceDisponible(11))
             {
                 w.calcul(11);
             }
             // Vérifie s'il reste au moins 0,05 euro à rendre
-            else if (w.compter - 0.05 >= 0.00 & w.pieceDisponible(12))
+            else if (transaction >= w.tabprix[12] & w.pieceDisponible(12))
             {
                 w.calcul(12);
             }
             // Vérifie s'il reste au moins 0,02 euro à rendre
-            else if (w.compter - 0.02 >= 0.00 & w.pieceDisponible(13))
+            else if (transaction >= w.tabprix[13] & w.pieceDisponible(13))
             {
                 w.calcul(13);
             }
             // Vérifie s'il reste au moins 0,01 euro à rendre
-            else if (w.compter - 0.01 >= 0.00 & w.pieceDisponible(14))
+            else if (transaction >= w.tabprix[14] & w.pieceDisponible(14))
             {
                 w.calcul(14);
             }
             else
             {
-                // Vérifie s'il reste suffisament d'argent pour atteindre le prix demandé
-                if(!w.resteMoney(w.compter))
-                {
-                    System.out.println("\n \u001B[31m" +" Veuillez nous excusez, il n'y a plus assez de pièces/billets pour atteindre le prix " +"\u001B[0m");
-                }
-                else
-                {
-                    System.out.println("\n" + "money : " + w.money);
-                    System.out.println("rendu : " + w.rendu);
-                    System.out.println("compter : " + w.compter);
-                    System.out.println("\u001B[31m" + "Il y a une erreur !" +"\u001B[0m");
-
-                }
+                System.out.println("\n \u001B[31m" +" Veuillez nous excusez, il n'y a plus assez de pièces/billets pour atteindre le prix " +"\u001B[0m");
                 break;
             }
 
             // Arrondi à la valeur supérieur afin de minimiser les approximations lors des calculs précédent
-            w.rendu = Math.round(w.rendu*100)/100d;
-            w.compter = Math.round(w.compter*100)/100d;
+            //w.rendu = Math.round(w.rendu*100)/100d;
+           // w.compter = Math.round(w.compter*100)/100d;
         }
 
         // chaine de caractère qui contient l'affichage final
@@ -173,7 +169,7 @@ public class Wallet
             }
         }
 
-        rep.append("\n").append("Soit un total de : ") .append(w.rendu).append("€");
+        rep.append("\n").append("Soit un total de : ") .append(w.rendu.getNumber()).append(" €");
         System.out.println(rep);
         sc.close();
     }
@@ -210,31 +206,12 @@ public class Wallet
     // Méthode pour calculer l'argent à rendre restant et pour modifier le nombre de pièce/billet limite disponible ainsi que le nombre de pièce/billet utilisé
     public void calcul(int i)
     {
-        compter = compter - tabprix[i];
-        rendu = rendu + tabprix[i];
-        tabmoney[i] = tabmoney[i] + 1;
-        tabmoneyLimite[i] = tabmoneyLimite[i] - 1;
-    }
+        this.moneyValue.setNumber(this.tabprix[i]);
+        this.compter.soustraction(this.moneyValue);
+        this.rendu.addition(this.moneyValue);
 
-
-    // Méthode qui permet de savoir s'il y a assez d'argent pour atteindre le prix
-    public boolean resteMoney(double rendre)
-    {
-        double reste = 0.00;
-
-        for (int i = 0; i < tabmoneyLimite.length; i++)
-        {
-            if(tabmoneyLimite[i] <= 0)
-            {
-                reste += 0;
-            }
-            else
-            {
-                reste += tabmoneyLimite[i] * tabprix[i];
-            }
-        }
-
-        return reste - rendre >= 0;
+        this.tabmoney[i] = this.tabmoney[i] + 1;
+        this.tabmoneyLimite[i] = this.tabmoneyLimite[i] - 1;
     }
 
 
